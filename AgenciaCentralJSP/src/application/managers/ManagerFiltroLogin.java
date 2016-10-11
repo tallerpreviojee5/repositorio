89749fe50a27aa0d679b4c89fc208d52ed3803c1
felipe <frivera@antel.com.uy>
2016-10-10 22:54:27 -0300

    Prueba

---
+++ AgenciaCentralJSP/src/application/managers/ManagerFiltroLogin.java
@@ -0,0 +1,69 @@
+package application.managers;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class ManagerFiltroLogin
+ */
+@WebFilter("/ManagerFiltroLogin")
+public class ManagerFiltroLogin implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerFiltroLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest R = (HttpServletRequest) request;		
+		if(R.getSession().getAttribute("autenticado") != null){
+			System.out.println("Usuario autenticado pasa filtro");	
+			chain.doFilter(request, response);
+		}
+		else{
+			String U = R.getRequestURI();
+			boolean intento_login = U.contains("login.html")|| U.contains("/ManagerLoginReportes");
+			if (intento_login){
+				chain.doFilter(request, response);
+			}
+			else{
+				RequestDispatcher rd = request.getRequestDispatcher("/login.html");
+				rd.forward(request, response);
+				System.out.println("Intento de acceso no autenticado");
+			}
+		}			
+
+		// pass the request along the filter chain
+		// chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ AgenciaCentralJSP/src/application/managers/ManagerLoginReportes.java
@@ -0,0 +1,68 @@
+package application.managers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import application.beans.UsuarioBean;
+
+/**
+ * Servlet implementation class ManagerLoginReportes
+ */
+@WebServlet("/ManagerLoginReportes")
+public class ManagerLoginReportes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UsuarioBean usuarioBean;
+	private ManagerUsuarios managerUsuarios;   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManagerLoginReportes() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String N = (String) request.getParameter("usuario");
+		String P = (String) request.getParameter("password");
+		System.out.println("Ingresa el Usuario: "+N);
+		System.out.println("con Password: "+P);
+		
+		usuarioBean = new UsuarioBean();
+        usuarioBean.setNombre(N);
+        usuarioBean.setPassword(P);
+        usuarioBean.setIdTerminal(0);
+        managerUsuarios = new ManagerUsuarios();
+		
+		if (managerUsuarios.validarUsuario(usuarioBean)){
+			// Autenticado OK
+			// Falta diferenciar problema Autenticacion de Autorizacion
+			RequestDispatcher rd = request.getRequestDispatcher("/opciones.html");
+			HttpSession S = request.getSession();
+			S.setAttribute("autenticado", true);
+			rd.forward(request, response);
+			
+		}else{
+			RequestDispatcher rd = request.getRequestDispatcher("/login.html");
+			rd.forward(request, response);
+		}
+	}
+
+}
